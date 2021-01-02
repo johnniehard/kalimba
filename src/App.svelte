c776fe085c189c14299e4f889a4b342c324c8b00
Johnnie Hard <johnnie@synvinkel.org>
2021-01-02 10:40:47 +0100

    Whitespace

--- src/App.svelte
+++ src/App.svelte
@@ -7,36 +7,34 @@
 	let loading = true;
 
 	const notes = [
-		{name: "D6", freq: 1174},
-		{name: "B5", freq: 987.7},
-		{name: "G5", freq: 783.9},
-		{name: "E5", freq: 659.2},
-		{name: "C5", freq: 523.2},
-		{name: "A4", freq: 440},
-		{name: "F4", freq: 349.2},
-		{name: "D4", freq: 293.6},
-		{name: "C4", freq: 261.6},
-		{name: "E4", freq: 329.6},
-		{name: "G4", freq: 391.9},
-		{name: "B4", freq: 493.8},
-		{name: "D5", freq: 587.3},
-		{name: "F5", freq: 698.4},
-		{name: "A5", freq: 880},
-		{name: "C6", freq: 1046},
-		{name: "E6", freq: 1318},
-
-	]
+		{ name: "D6", freq: 1174 },
+		{ name: "B5", freq: 987.7 },
+		{ name: "G5", freq: 783.9 },
+		{ name: "E5", freq: 659.2 },
+		{ name: "C5", freq: 523.2 },
+		{ name: "A4", freq: 440 },
+		{ name: "F4", freq: 349.2 },
+		{ name: "D4", freq: 293.6 },
+		{ name: "C4", freq: 261.6 },
+		{ name: "E4", freq: 329.6 },
+		{ name: "G4", freq: 391.9 },
+		{ name: "B4", freq: 493.8 },
+		{ name: "D5", freq: 587.3 },
+		{ name: "F5", freq: 698.4 },
+		{ name: "A5", freq: 880 },
+		{ name: "C6", freq: 1046 },
+		{ name: "E6", freq: 1318 },
+	];
 	const sampler = new Tone.Sampler({
 		urls: {
-			"C4": "C4.wav",
+			C4: "C4.wav",
 		},
 		baseUrl: "/",
-	}).toDestination()
+	}).toDestination();
 
 	Tone.loaded().then(() => {
-		loading = false
-	})
-
+		loading = false;
+	});
 </script>
 
 <style>
@@ -67,7 +65,7 @@
 	<button
 		on:click|preventDefault={async () => {
 			await Tone.start();
-			document.documentElement.requestFullscreen()
+			document.documentElement.requestFullscreen();
 			started = true;
 		}}>
 		Start Tone
@@ -76,7 +74,7 @@
 	<div class="kalimba">
 		<div class="tines">
 			{#each notes as note}
-				<Tine {note} {sampler}/>
+				<Tine {note} {sampler} />
 			{/each}
 		</div>
 	</div>
