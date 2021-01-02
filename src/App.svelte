107ca0d425adcfd507120b0d1f7c1b05f704f556
Johnnie Hard <johnnie@synvinkel.org>
2021-01-02 08:22:06 +0100

    First basic layout done

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+</script>
+
+<div class="kalimba">
+	<div class="tines">
+		<div class="tine">D</div>
+		<div class="tine">C</div>
+		<div class="tine">E</div>
+	</div>
+</div>
+
+<style>
+	.kalimba {
+		display: flex;
+		justify-content: center;
+		background: salmon;
+		height: 100%;
+	}
+
+	.tines {
+		background: orange; 
+		align-self: start;
+		justify-content: center;
+		padding: 20px;
+		padding-top: 0;
+		display: flex;
+		align-items: start;
+		border-radius: 0 0 30px 30px;
+		gap: 10px;
+
+	}
+	
+	.tine {
+		background: peachpuff;
+		height: 100px;
+		display: flex;
+		align-items: end;
+		padding: 10px;
+		color: tomato;
+		font-weight: bold;
+		border-radius: 0 0 10px 10px;
+	}
+</style>
